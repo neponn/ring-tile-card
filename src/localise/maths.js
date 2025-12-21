bad12301fa4a4c012812f074befa1ed260a596d3
Nick <32885870+neponn@users.noreply.github.com>
2025-12-21 13:27:42 +1100

    Change local detection from browser to hass

--- src/localise/maths.js
+++ src/localise/maths.js
@@ -1,11 +1,19 @@
 export function getDecimalSeparator(locale) {
-  const parts = new Intl.NumberFormat(locale).formatToParts(1.1);
-  const dec = parts.find((p) => p.type === "decimal");
-  return dec ? dec.value : ".";
+  let dp = ".";
+  // figure out what the locale has to say
+  if (locale?.number_format === "language") {
+    // set according to the language
+    const parts = new Intl.NumberFormat(locale.language).formatToParts(1.1);
+    dp = parts.find((p) => p.type === "decimal").value;
+  } else if (locale?.number_format.endsWith("comma")) {
+    // custom number format
+    dp = ",";
+  }
+  return dp;
 }
 
-export function toLocaleFixed(value, decimals) {
+export function toLocaleFixed(value, decimals, locale) {
   const fixedValue = parseFloat(value).toFixed(decimals);
-  const dp = getDecimalSeparator();
+  const dp = getDecimalSeparator(locale);
   return fixedValue.replace(".", dp);
 }
